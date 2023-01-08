d933e337f4fa78c829e94bf013b3be87dc57a951
Crategus <kaiser@crategus.com>
2023-01-08 13:40:47 +0100

    Initial commit of files for generating the documentation

---
+++ doc/liber-html-single.lisp
@@ -0,0 +1,5 @@
+(load "liber.lisp")
+(in-package :liber-gdk-pixbuf)
+(generate-html-single-page)
+
+;;; 2022-11-28
---
+++ doc/liber-html.lisp
@@ -0,0 +1,6 @@
+(load "liber.lisp")
+(in-package :liber-gdk-pixbuf)
+(generate-html-single-page)
+(generate-html)
+
+;;; 2022-11-28
