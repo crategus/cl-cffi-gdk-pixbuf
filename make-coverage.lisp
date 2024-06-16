3080ea11682b92a266df361da22de3eb764d8ade
Crategus <kaiser@crategus.com>
2024-06-16 20:51:56 +0200

    Add file to make code coverage for the testsuite

---
+++ make-coverage.lisp
@@ -0,0 +1,6 @@
+(require :sb-cover)
+(declaim (optimize sb-cover:store-coverage-data))
+(asdf:load-system :cl-cffi-gdk-pixbuf :force t)
+(declaim (optimize (sb-cover:store-coverage-data 0)))
+(asdf:test-system :cl-cffi-gdk-pixbuf)
+(sb-cover:report (glib-sys:sys-path "test/report/" "cl-cffi-gdk-pixbuf"))
